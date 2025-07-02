d8378bd3e85c08b4f3ab2814844e4583cb0ce3d1
lidia-freitas <lidiafreitas.me@gmail.com>
2025-07-02 19:50:55 -0300

    initialize React + TypeScript + Vite project with essential configuration and components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import lumiereLogo from './assets/logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <img src={lumiereLogo} className="logo lumiere" alt="Lumiere logo" />
+      <h1>Lumiere Web Front</h1>
+    </div>
+  );
+}
+
+export default App;
